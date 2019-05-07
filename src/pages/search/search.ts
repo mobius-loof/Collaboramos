b594c0af129078a73fb018ef3c05750b93b3ad99
lishane31 <sil218@ucsd.edu>
2019-05-07 13:44:12 -0700

    Added msgs and some functionality to home

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -15,27 +15,9 @@ export class SearchPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public items: Items) { }
 
-  /**
-   * Perform a service for the proper items.
-   */
-  getItems(ev) {
-    let val = ev.target.value;
-    if (!val || !val.trim()) {
-      this.currentItems = [];
-      return;
-    }
-    this.currentItems = this.items.query({
-      name: val
-    });
-  }
 
-  /**
-   * Navigate to the detail page for this item.
-   */
-  openItem(item: Item) {
-    this.navCtrl.push('ItemDetailPage', {
-      item: item
-    });
+  viewMessages() {
+    this.navCtrl.push('MessagePage');
   }
 
 }
