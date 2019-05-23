9a94ca9706d36e16d2f2dc8b95c5731f6ae832da
thomastangerine <thomastang.thomas97@gmail.com>
2019-05-22 17:38:35 -0700

    CRUD for Account, Project, Candidate, Channels

--- src/providers/firestore/firestore.ts
+++ src/providers/firestore/firestore.ts
@@ -1,12 +1,6 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore, AngularFirestoreDocument, Reference, DocumentReference } from 'angularfire2/firestore';
-// import { Account } from '../models/account'
-// import { Project } from '../models/project'
-import { Candidate, Project, Account } from '../../models'
-import { async } from 'rxjs/internal/scheduler/async';
-import { Subscribable, Subscription } from 'rxjs';
-import { SelectPopover } from 'ionic-angular';
-import { Observable } from 'rxjs';
+import { AngularFirestore, DocumentReference } from 'angularfire2/firestore';
+import { Candidate, Project, Account, Channel } from '../../models'
 
 /*
   Generated class for the FirestoreProvider provider.
@@ -20,29 +14,94 @@ export class Firestore {
   constructor(public firestore: AngularFirestore) { }
   // Account CRUD
 
-  // Create account
-  /*createAccount(model: Account): Promise<void> {
+  // Create Account
+  createAccount(model: Account): Promise<void> {
     const id = this.firestore.createId(); // generate an ID
 
     // create the account in Firestoer
     return this.firestore.doc(`accounts/${id}`).set({
       address: model.address,
-      candidate_id: can
-    })
-  }*/
+      candidate_id: model.candidate_id,
+      email: model.email,
+      first_name: model.first_name,
+      last_name: model.last_name,
+      phone_number: model.phone_number,
+      profile_img: model.profile_image,
+      project_id: model.project_id
+    });
+  }
+
+  // Read Account
   getAccount(id: string): Promise<any> {
     return this.firestore.collection('accounts').doc(id).ref.get().then(doc => {
       return doc.data();
     });
   }
 
-  // Candidate Profile CRUD
-  getCandidateProfile(id: string): AngularFirestoreDocument<Candidate> {
-    return this.firestore.collection('candidate_profiles').doc(id);
+  // Update Account
+  updateAccount(id: string, model: Account): Promise<void> {
+    
+    return this.firestore.collection('accounts').doc(id).update({
+      address: model.address,
+      candidate_id: model.candidate_id,
+      email: model.email,
+      first_name: model.first_name,
+      last_name: model.last_name,
+      phone_number: model.phone_number,
+      profile_img: model.profile_image,
+      project_id: model.project_id
+    });
   }
 
-  updateCandidateProfile(id: string): Promise<Candidate> {
-    return null;
+  // Delete Account
+  deleteAccount(id: string): Promise<void> {
+    return this.firestore.collection('accounts').doc(id).delete();
+  }
+
+  // Candidate Profile CRUD
+
+  // Create Candidate
+  createCandidate(model: Candidate): Promise<void> {
+    const id = this.firestore.createId();
+
+    return this.firestore.doc(`candidate_profiles/${id}`).set({
+      id: id,
+      description: model.description,
+      files: model.files,
+      images: model.images,
+      is_visible: model.is_visible,
+      tags: model.tags
+    });
+  }
+
+  // Read Candidate via ID
+  getCandidateProfile(id: string): Promise<any> {
+    return this.firestore.collection('candidate_profiles').doc(id).ref.get().then(doc=> {
+      return doc.data();
+    });
+  }
+
+  // Read Candidate via Reference
+  getCandidateProfile(ref: DocumentReference): Promise<any> {
+    return ref.get().then(doc => {
+      return doc.data();
+    })
+  }
+
+  // Update Candidate
+  updateCandidateProfile(id: string, model: Candidate): Promise<void> {
+    return this.firestore.doc(`candidate_profiles/${id}`).update({
+      description: model.description,
+      files: model.files,
+      images: model.images,
+      is_visible: model.is_visible,
+      tags: model.tags
+    });
+  }
+
+  // Delete Candidate
+  deleteCandidateProfile(id: string): Promise<void> {
+    return this.firestore.collection('candidate_profiles').doc(id).delete();
   }
 
   // Project Profile CRUD
@@ -54,6 +113,7 @@ export class Firestore {
     // Returns promise of success/failure for creating the project document on Firestore
     return this.firestore.doc(`project_profiles/${id}`).set({
       description: model.description,
+      id: id,
       images: model.images,
       is_visible: model.is_visible,
       proj_name: model.proj_name,
@@ -62,12 +122,15 @@ export class Firestore {
 
   }
 
-  // Read Profile
-  getProjectProfile(id: string): AngularFirestoreDocument<Project> {
+  // Read Profile via ID
+  getProjectProfile(id: string): Promise<any> {
     // Returns reference of the document to read
-    return this.firestore.collection('project_profiles').doc(id);
+    return this.firestore.collection('project_profiles').doc(id).ref.get().then(doc => {
+      return doc.data();
+    });
   }
 
+  // Read Profile via Reference
   getProjectProfile(ref: DocumentReference): Promise<any> {
     return ref.get().then(doc=> {
       return doc.data();
@@ -90,5 +153,40 @@ export class Firestore {
   deleteProjectProfile(id: string): Promise<void> {
     // Returns promise of success/failure for deleting the project document on Firestore
     return this.firestore.doc(`project_profiles/${id}`).delete();
+  }
+
+  // Channel CRUD
+  
+  // Create Channel
+  createChannel(model: Channel): Promise<void> {
+    const id = this.firestore.createId();
+
+    return this.firestore.doc(`channels/${id}`).set({
+      id: id,
+      last_message_sent: model.last_message_sent,
+      media: model.media,
+      members: [model.chat_member_candidate, model.chat_member_project]
+    });
+  }
+
+  // Read Channel
+  getChannel(id: string): Promise<any> {
+    return this.firestore.collection('channels').doc(id).ref.get().then(doc=> {
+      return doc.data();
+    });
+  }
+
+  // Update Channel
+  updateChannel(id: string, model: Channel): Promise<void> {
+    return this.firestore.doc(`channels/${id}`).update({
+      last_message_sent: model.last_message_sent,
+      media: model.media,
+      members: [model.chat_member_candidate, model.chat_member_project]
+    });
+  }
+
+  // Delete Channel
+  deleteChannel(id: string): Promise<void> {
+    return this.firestore.collection('channels').doc(id).delete();
   }
 }
