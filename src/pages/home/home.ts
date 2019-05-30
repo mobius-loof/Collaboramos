4ed5102f5d3e0978a1397ecd918b76b6618543b4
Shane <sil218@ucsd.edu>
2019-05-29 22:24:43 -0700

    Yay fixed tags

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild, ViewChildren, QueryList, Renderer} from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, MenuController } from 'ionic-angular';
 import { Http } from '@angular/http';
+import { MyApp } from '../../app/app.component';
 import 'rxjs/Rx';
 
 import {
@@ -22,8 +23,9 @@ import { Items } from '../../providers';
 })
 export class HomePage {
   mode = "candidate";
-  constructor(public navCtrl: NavController, public navParams: NavParams, public items: Items, private http: Http, public renderer: Renderer) {
-    };
+  constructor(public navCtrl: NavController, public navParams: NavParams, public items: Items, private http: Http, public renderer: Renderer, public appCom: MyApp, private menuCtrl: MenuController) {
+    this.menuCtrl.swipeEnable(false);   
+  };
 
   viewMessages() {
     this.navCtrl.push('MessagePage');
@@ -34,5 +36,14 @@ export class HomePage {
    */
   switchMode() {
     this.mode = this.mode==="candidate" ? "project" : "candidate";
+    console.log(this.mode)
   }
+
+  openMenu() {
+    if(!this.menuCtrl.isOpen() && this.appCom.isEdit()) {
+      this.appCom.toggleProfileSettings();
+    }
+    this.menuCtrl.open();
+  }
+
 }
