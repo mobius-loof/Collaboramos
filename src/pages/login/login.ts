33f5ec77cb584e6ff9fb49e0de7e729472536aff
gypang <gypang24@gmail.com>
2019-05-31 10:12:20 -0700

    Add loading controller for improved UI

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, ToastController } from 'ionic-angular';
+import { IonicPage, NavController, ToastController, LoadingController } from 'ionic-angular';
 
 import { User, Auth, Firestore } from '../../providers';
 import { MainPage } from '../';
@@ -24,7 +24,9 @@ export class LoginPage {
     public user: User,
     public toastCtrl: ToastController,
     private auth: Auth,
-    private firestore: Firestore) {}
+    private firestore: Firestore,
+    private loadingCtrl: LoadingController
+  ) {}
 
   cancel() {
     this.navCtrl.pop()
@@ -33,6 +35,11 @@ export class LoginPage {
   // Attempt to login in through our User service
   doLogin() {
     let params = {};
+
+    let loading = this.loadingCtrl.create({
+      content: 'Logging in...'
+    });
+    loading.present();
 
     this.auth.login(this.credentials).then((user) => {
       return this.firestore.getAccount(user.user.uid);
@@ -54,9 +61,9 @@ export class LoginPage {
     }).then ( candidateProfile => {
       params['candidateProfile'] = candidateProfile;
     }).then( _ => {
+      loading.dismiss();
       if (params['candidateProfile'] == null && params['projectProfile'] == null) {
         // take them to the profile creation page
-        // TODO early exit from promise
       } else {
         this.navCtrl.setRoot(MainPage, params);
         this.showLoginSuccess();     
