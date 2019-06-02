bc7e0bdb9b3a7fce834c37de04358f608116bbb8
vivianweiii <law045@ucsd.edu>
2019-06-01 17:57:37 -0700

    working on making messages prettier

--- src/models/channel.ts
+++ src/models/channel.ts
@@ -2,12 +2,7 @@ import { Project, Candidate, Message } from "./";
 import { DocumentReference } from "angularfire2/firestore";
 
 export interface Channel {
-    chatMemberProject: Project,
-    chatMemberCandidate: Candidate,
-    chat_member_project: DocumentReference,
-    chat_member_candidate: DocumentReference,
-    lastMessageSent: Message,
-    last_message_sent: DocumentReference,
-    recentMessagesSent: Message[],
-    media: string[]
+    id: string,
+    last_message_sent: string,
+    members: string[]
 }
