2d9dd86cd11fed38abf77fc00b6655b28ddfe787
gypang <gypang24@gmail.com>
2019-06-02 23:28:04 -0700

    Revamp menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -56,21 +56,14 @@ export class MyApp {
   private isToggled: boolean;
 
   //variables related to project profile
-  private projectCreated: boolean;
   private projectInvis: boolean;
   private projectColor: string = this.PROJECT_COLOR;
 
   //variables related to candidate profile
-  private candidateCreated: boolean;
   private candidateInvis: boolean;
   private candidateColor: string = this.CANDIDATE_COLOR;
 
-  //boolean value to check if ion-toggle is set
-  //private checked: boolean;
-
-  //variables related to edit button status
-  private editButton: string = 'Edit';
-  private editMode: boolean = false;
+  private isEdit: boolean = false;
 
   //variable to tell what was last profile
   private currentProfile: string;
@@ -80,9 +73,6 @@ export class MyApp {
   private account: Account;
   private projectRef: DocumentReference;
   private candidateRef: DocumentReference;
-
-  private projToggled: boolean;
-  private candToggled: boolean;
 
   constructor(private platform: Platform,
               settings: Settings,
@@ -101,104 +91,33 @@ export class MyApp {
     });
   }
 
+  // this should handle input from ion-toggles
   public notify(check: boolean) {
     //need to interface with Firebase for this to remember last toggled setting
-    //this.checked = !check;
-    this.isToggled = !check;
+    // this.isToggled = !check;
 
-    if(this.currentProfile === this.PROJECT) {
+    // if(this.currentProfile === this.PROJECT) {
 
-      if(this.projToggled) {
-        this.project = this.switchProjectVisibleModel(this.project, false);
+    //   if(this.projToggled) {
+    //     this.project = this.switchProjectVisibleModel(this.project, false);
 
-        this.firestore.updateProjectProfile(this.project);
-      } else if(!this.projToggled) {
+    //     this.firestore.updateProjectProfile(this.project);
+    //   } else if(!this.projToggled) {
 
-        this.project = this.switchProjectVisibleModel(this.project, true);
+    //     this.project = this.switchProjectVisibleModel(this.project, true);
 
-        this.firestore.updateProjectProfile(this.project);
-      }
-      //this.projectSettings();
-    } else if(this.currentProfile === this.CANDIDATE) {
-      this.isToggled = false;
-    }
+    //     this.firestore.updateProjectProfile(this.project);
+    //   }
+    //   //this.projectSettings();
+    // } else if(this.currentProfile === this.CANDIDATE) {
+    //   this.isToggled = false;
+    // }
   }
-
-  /*openPage(page) {
-    // Reset the content nav to have just this page
-    // we wouldn't want the back button to show in this scenario
-    this.nav.setRoot(page.component);
-  }*/
 
   closeMenu() {
     //closes the left side menu
     this.menuCtrl.close();
-
-    if(this.editMode) {
-      this.toggleProfileSettings();
-    }
-  }
-
-  toggleEditSettings() {
-    this.editMode = !this.editMode;
-    this.toggleProfileSettings();
-  }
-
-  toggleProfileSettings() {
-    //change the editMode boolean, meaning pressed button
-
-    //if in edit mode, then change the profile tabs to red indicating delete
-    if(this.editMode) {
-      this.projectColor = 'danger';
-      this.candidateColor = 'danger';
-
-      this.editButton = 'Cancel';
-
-    //otherwise if hit cancel, then set the profile to last one used
-    } else if(!this.editMode) {
-      this.editButton = 'Edit';
-
-      if(this.projectCreated && !this.candidateCreated) {
-        this.currentProfile = this.PROJECT;
-      } else if(!this.projectCreated && this.candidateCreated) {
-        this.currentProfile = this.CANDIDATE;
-      }
-
-      if(this.currentProfile === this.PROJECT) {
-        this.projectColor = this.PROJECT_COLOR;
-
-        if(this.candidateCreated) {
-          this.candidateColor = this.NON_COLOR;
-        }
-      } else if(this.currentProfile === this.CANDIDATE) {
-        this.candidateColor = this.CANDIDATE_COLOR;
-
-        if(this.projectCreated) {
-          this.projectColor = this.NON_COLOR;
-        }
-
-      }
-
-    }
-  }
-
-  clickedProfile(profileType) {
-    //if project button tapped, then create/delete settings
-    if(profileType === this.PROJECT) {
-      this.projectSettings();
-
-      if(this.editMode && this.projectCreated) {
-        this.promptDelete(this.PROJECT);
-      }
-
-    //if candidate button tapped, then create/delete settings
-    } else if(profileType === this.CANDIDATE) {
-      this.candidateSettings();
-
-      if(this.editMode && this.candidateCreated) {
-        this.promptDelete(this.CANDIDATE);
-      }
-    }
+    this.isEdit = false;
   }
 
   changePage(item) {
@@ -207,129 +126,7 @@ export class MyApp {
     this.closeMenu();
   }
 
-  projectCreate() {
-    if(!this.projectCreated && !this.editMode) {
-
-      this.nav.push('CreateProjectPage', {
-        account: this.account //,
-      });
-
-      this.projectCreated = true;
-      this.projectInvis = false;
-
-      //CHANGE THE NAME BASED ON FIRESTORE
-      this.projectColor = this.PROJECT_COLOR;
-      this.currentProfile = this.PROJECT;
-
-      if(this.candidateCreated) {
-        this.candidateColor = this.NON_COLOR;
-      }
-
-      this.closeMenu();
-    }
-  }
-
-  candidateCreate() {
-    if(!this.candidateCreated && !this.editMode) {
-      this.nav.push('CreateCandidatePage', {
-        account: this.account //,
-      });
-      /*this.nav.setRoot('CreateCandidatePage', {
-        account: this.account,
-        candidateProfile: this.candidate,
-        projectProfile: this.candidate,
-        candidateProfileRef: null,
-        projectProfileRef: null
-      });
-
-      this.candidate = {
-        id: '32kul1tAw9FJRUC98hhg',
-        name: 'Gary Bary',
-        image: '',
-        website: 'google',
-        resume_URL: '',
-        is_visible: true,
-        skills: [],
-        description: '',
-        chats: {},
-        interests: {},
-        matches: {},
-        waitlist: [],
-        phone_number: '',
-        email: '',
-        address: ''
-      };*/
-
-      this.candidateCreated = true;
-      this.candidateInvis = false;
-
-      this.candidateColor = this.CANDIDATE_COLOR;
-      this.currentProfile = this.CANDIDATE;
-
-      //CHANGE THE NAME BASED ON FIRESTORE
-
-      if(this.projectCreated) {
-        this.projectColor = this.NON_COLOR;
-      }
-      this.closeMenu();
-    }
-  }
-
-  projectSettings() {
-    //set the defaults for the project profile once it is created
-    if(!this.editMode) {
-      this.currentProfile = 'project';
-      this.projectColor = this.PROJECT_COLOR;
-
-      if(this.candidateCreated) {
-        this.candidateColor = this.NON_COLOR;
-      }
-
-      this.projectPublishEvents();
-    }
-  }
-
-  candidateSettings() {
-    //set defaults for candidate profile once it has been created
-    if(!this.editMode) {
-      this.currentProfile = 'candidate';
-      this.candidateColor = this.CANDIDATE_COLOR;
-
-      if(this.projectCreated) {
-        this.projectColor = this.NON_COLOR;
-      }
-
-      this.candidatePublishEvents();
-    }
-  }
-
-  profileDelete(profileType) {
-    //if tap delete on prompt for project profile, then delete
-    if(profileType === this.PROJECT) {
-      this.projectCreated = false;
-      this.projectInvis = false;
-
-      if(this.candidateCreated) {
-        this.currentProfile = 'candidate';
-      } else {
-        this.currentProfile = '';
-      }
-
-    //if tap delete on prompt for candidate profile, then delete
-    } else if(profileType === this.CANDIDATE) {
-      this.candidateCreated = false;
-      this.candidateInvis = false;
-
-      if(this.projectCreated) {
-        this.currentProfile = 'project';
-      } else {
-        this.currentProfile = '';
-      }
-    }
-
-    this.reset();
-  }
-
+  
   promptDelete(profileType) {
     //bring up a prompt to delete profile
     let alert = this.alertCtrl.create({
@@ -346,7 +143,7 @@ export class MyApp {
         {
           text: 'Delete',
           handler: () => {
-            this.profileDelete(profileType);
+            // this.profileDelete(profileType);
           }
         }
       ]
@@ -355,9 +152,8 @@ export class MyApp {
   }
 
   reset() {
-    if(!this.projectCreated && !this.candidateCreated) {
-      this.editMode = false;
-      this.editButton = 'Edit';
+    if(!this.project && !this.candidate) {
+      this.isEdit = false;
 
       // NAVPARAMS?
       this.nav.setRoot('CreateProfilePage');
@@ -365,30 +161,20 @@ export class MyApp {
     }
   }
 
-  setEdit(editName: string) {
-    this.editButton = editName;
+  setEdit(edit: boolean) {
+    this.isEdit = edit;
   }
 
   setAccount(acc: Account) {
     this.account = acc;
   }
 
-  setCandidateProfile(candy: Candidate) {
-    this.candidate = candy;
-    if (candy == null) {
-      this.candidateCreated = false;
-    } else {
-      this.candidateCreated = true;
-    }
+  setCandidateProfile(candidate: Candidate) {
+    this.candidate = candidate;
   }
 
   setProjectProfile(proj: Project) {
     this.project= proj;
-    if (proj == null) {
-      this.projectCreated = false;
-    } else {
-      this.projectCreated = true;
-    }
   }
 
   setCandidateProfileRef(ref: DocumentReference) {
@@ -399,17 +185,16 @@ export class MyApp {
     this.candidateRef = ref;
   }
 
-  isEdit() {
-    //return the status of the editMode
-    return this.editMode;
-  }
+  setCurrentProfile(profile: string) {
+    this.currentProfile = profile;
 
-  setCurrentProfile(prof: string) {
-    this.currentProfile = prof;
-
-    if(prof === this.PROJECT) {
+    if(profile === 'project') {
+      this.projectColor = "project_banner";
+      this.candidateColor = "nop";
       this.projectPublishEvents();
-    } else if(prof === this.CANDIDATE) {
+    } else if(profile === 'candidate') {
+      this.projectColor = "nop";
+      this.candidateColor = "candy_banner";
       this.candidatePublishEvents();
     }
   }
@@ -420,26 +205,6 @@ export class MyApp {
 
   projectPublishEvents() {
     this.events.publish('currentProfile', 'project');
-  }
-
-  switchProjectVisibleModel(model: Project, vis: boolean) {
-    return {
-      id: model.id,
-      name: model.name,
-      image: model.image,
-      description: model.description,
-      is_visible: vis,
-      frameworks: model.frameworks,
-      skills: model.skills,
-      chats: model.chats,
-      interests: model.interests,
-      matches: model.matches,
-      waitlist: model.waitlist,
-      address: model.address,
-      email: model.email,
-      website: model.website,
-      phone_number: model.phone_number
-    };
   }
 
 }
