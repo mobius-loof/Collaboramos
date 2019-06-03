f73d370f0bc345eaa9678fd4d2d8af92baa22424
gypang <gypang24@gmail.com>
2019-06-03 00:11:28 -0700

    fix components export

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -5,6 +5,7 @@ import { HttpModule } from '@angular/http';
 import { SwingModule } from 'angular2-swing';
 import { HomeProjectComponent } from './home-project/home-project';
 import { HomeCandidateComponent } from './home-candidate/home-candidate';
+import { TimelineComponentModule } from './timeline/timeline.module';
 
 @NgModule({
 	declarations: [
@@ -36,4 +37,23 @@ export class HomeCandidateModule{}
 })
 export class HomeProjectModule{}
 
+import { Timer } from './countdown-timer/timer';
+import { TimerProgress } from './timer-progress/timer-progress';
+import { ExpandableHeader } from './expandable-header/expandable-header';
+import { FlashCardComponent } from './flash-card/flash-card';
+import { AccordionListComponent } from './accordion-list/accordion-list';
 
+export const components = [
+  Timer,
+  TimerProgress,
+  ExpandableHeader,
+  FlashCardComponent,
+  AccordionListComponent,
+];
+
+@NgModule({
+  declarations: [components],
+  imports: [IonicModule],
+  exports: [components, TimelineComponentModule]
+})
+export class ComponentsModule {}
