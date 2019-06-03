e057e9df701c861593ada789cc68961b70404c5e
thomastangerine <thomastang.thomas97@gmail.com>
2019-06-02 23:45:02 -0700

    fixed merge conflict

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,5 @@
 // The page the user lands on after opening the app and without a session
-export const FirstRunPage = 'TabsPage';
+export const FirstRunPage = 'LoginPage';
 
 // The main page the user will see as they use the app over a long period of time.
 // Change this if not using tabs
